9e174b2c6daaefab5d2c43f226395e2df320369f
Igor Bobro <igor.bobro.lul@gmail.com>
2022-01-12 19:35:16 +0200

    project

---
+++ Catalog/Catalog.Host/Controllers/CatalogTypeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using Catalog.Host.Models.Requests;
+using Catalog.Host.Models.Response;
+using Catalog.Host.Services.Interfaces;
+using Infrastructure.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Host.Controllers;
+
+[ApiController]
+[Route(ComponentDefaults.DefaultRoute)]
+public class CatalogTypeController : ControllerBase
+{
+    private readonly ILogger<CatalogTypeController> _logger;
+    private readonly ICatalogTypeService _catalogTypeService;
+
+    public CatalogTypeController(
+        ILogger<CatalogTypeController> logger,
+        ICatalogTypeService catalogTypeService)
+    {
+        _logger = logger;
+        _catalogTypeService = catalogTypeService;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(AddTypeResponse<int>), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult> CreateProduct(CreateTypeRequest request)
+    {
+        var result = await _catalogTypeService.CreateTypeAsync(request.Type);
+        return Ok(new AddItemResponse<int>() { Id = result });
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(DeleteTypeResponse<int>), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult> Delete(int id)
+    {
+        await _catalogTypeService.DeleteProductAsync(id);
+        return Ok(new DeleteTypeResponse<int>() { Id = id });
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(UpdateItemResponse<int>), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult> Update(UpdateProductRequest request)
+    {
+        await _catalogTypeService.UpdateProductAsync(request.Id, request.Name);
+        return Ok(new UpdateItemResponse<int>() { Id = request.Id });
+    }
+}
---
+++ Catalog/Catalog.Host/Repositories/CatalogBrandRepository.cs
@@ -0,0 +1,40 @@
+﻿using Catalog.Host.Data;
+using Catalog.Host.Data.Entities;
+using Catalog.Host.Repositories.Interfaces;
+using Catalog.Host.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Host.Repositories
+{
+    public class CatalogBrandRepository : ICatalogBrandRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CatalogBrandRepository(
+            IDbContextWrapper<ApplicationDbContext> dbContextWrapper)
+        {
+            _dbContext = dbContextWrapper.DbContext;
+        }
+
+        public async Task<IEnumerable<CatalogBrand>> GetBrandsAsync()
+        {
+            var items = await _dbContext.CatalogBrands.ToListAsync();
+            return items;
+        }
+
+        public Task<int> Add(string brand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(int id, string brand)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
